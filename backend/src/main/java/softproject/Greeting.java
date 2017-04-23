917753456efbcd7b07f340c02b44f72b823fe02a
Pontus Stjernström <pontus@stjernstrom.nu>
2017-04-23 21:01:23 +0200

    Tested adding a simple RestController class. Tried two different methods, one returning json and the other a simple string. Both worked! This means we can simply choose to either go by our own rest API or do the more MVC approach.

---
+++ backend/src/main/java/softproject/Greeting.java
@@ -0,0 +1,22 @@
+package softproject;
+
+/**
+ * Created by Pontus on 2017-04-23.
+ */
+public class Greeting {
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
---
+++ backend/src/main/java/softproject/controllers/TestRestController.java
@@ -0,0 +1,31 @@
+package softproject.controllers;
+
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import softproject.Greeting;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * Created by Pontus on 2017-04-23.
+ */
+
+@RestController
+public class TestRestController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/test")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="there") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                String.format(template, name));
+    }
+
+    @RequestMapping("/test2")
+    public String greeting() {
+        return "I come from java.";
+    }
+}
