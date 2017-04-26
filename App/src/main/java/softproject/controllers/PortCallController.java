a0831589803a4bb5f4137bfc1c0aad6c75db5b9b
Pontus Stjernström <pontus@stjernstrom.nu>
2017-04-26 10:32:17 +0200

    Added a simple static class for http-requests. For now used to display a raw list of port calls to our front end. That means our acceptance criteria is reached for this story.

---
+++ App/src/main/java/softproject/controllers/PortCallController.java
@@ -0,0 +1,19 @@
+package softproject.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import softproject.HTTPRequests;
+
+/**
+ * Created by pontu on 2017-04-26.
+ */
+
+@RestController
+public class PortCallController {
+
+    @RequestMapping("/port_calls")
+    public String getPortCalls(){
+        //Detta är Pontuss VBoxs superfina IP
+        return HTTPRequests.executeGet("http://192.168.56.101:8080/dmp/port_calls");
+    }
+}
