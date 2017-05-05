a53b121088fd1c61402d368c339c5e8750c8ede4
Pontus Stjernström <pontus@stjernstrom.nu>
2017-05-05 12:10:27 +0200

    Added a mapping /portcall?id=xx for getting a simple .json that we can use for our html from js.

--- App/src/main/java/softproject/controllers/PortCallController.java
+++ App/src/main/java/softproject/controllers/PortCallController.java
@@ -1,6 +1,7 @@
 package softproject.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import softproject.HTTPRequests;
 
@@ -15,5 +16,30 @@ public class PortCallController {
     public String getPortCalls(){
         //Detta är Pontuss VBoxs superfina IP
         return HTTPRequests.executeGet("http://192.168.56.101:8080/dmp/port_calls?count=1");
+    }
+
+    @RequestMapping("/portcall")
+    public DummyPortCall portcall(@RequestParam(value="id") String id) {
+        int portId = Integer.parseInt(id);
+        //Plocka ur lista egentligen
+        return new DummyPortCall();
+    }
+
+    private class DummyPortCall{
+        private int id;
+        private String vesselName;
+
+        public DummyPortCall(){
+            id = 1337;
+            vesselName = "Superfint anrop";
+        }
+
+        public int getId(){
+            return id;
+        }
+
+        public String getVesselName(){
+            return vesselName;
+        }
     }
 }
