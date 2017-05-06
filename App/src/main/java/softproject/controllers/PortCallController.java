5bec6fab089af1fd47baeec2599d7e057edfe65e
Johan Berndtsson <johanbrndtssn@gmail.com>
2017-05-06 16:21:57 +0200

    fortsatt med att skicka in portcalls

--- App/src/main/java/softproject/controllers/PortCallController.java
+++ App/src/main/java/softproject/controllers/PortCallController.java
@@ -1,5 +1,7 @@
 package softproject.controllers;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import softproject.HTTPRequests;
@@ -11,9 +13,10 @@ import softproject.HTTPRequests;
 @RestController
 public class PortCallController {
 
-    @RequestMapping("/port_calls")
-    public String getPortCalls(){
-        //Detta är Pontuss VBoxs superfina IP
-        return HTTPRequests.executeGet("http://192.168.56.101:8080/dmp/port_calls?count=1");
+    @PostMapping("/portcalls/add")
+    public void addPortCall(@RequestBody String newPortCall) {
+        System.out.println(newPortCall);
+
     }
+
 }
--- App/src/main/resources/static/js/unscheduled.js
+++ App/src/main/resources/static/js/unscheduled.js
@@ -1,7 +1,32 @@
-var addPortcallButt0on = $('#add-portcall-button');
+var addPortcallButton = $('#add-portcall-button');
+// var submitPortcall = $('#submit-portcall-button');
 var addPortCallForm = $('.add-portcall-input');
-
 
 addPortcallButton.on('click', function () {
     addPortCallForm.removeClass('hidden');
+});
+
+addPortCallForm.on('submit', function(){
+    var cargoInVal = $('#cargo-in-input').val();
+    var cargoOutVal = $('#cargo-out-input').val();
+
+    var newPortCall = {
+        cargoIn : cargoInVal,
+        cargoOut : cargoOutVal
+    };
+
+    // $.post('http://locahost:8080/portcalls/add', newPortCall, function () {
+    //     alert("Portcall sent");
+    // });
+
+    $.ajax({
+        url: 'http://localhost:8080/portcalls/add',
+        type: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify(newPortCall),
+        dataType: 'json'
+    });
+
+
+    return false;
 });
