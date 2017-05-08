c3fa55ab98b2617e2a68e656652de92c26c28889
Pontus Stjernström <pontus@stjernstrom.nu>
2017-05-08 18:57:23 +0200

    The feed is tested and now WORKS FRONTEND WISE! It displays some parts of the portcallmessage. I have problems with interpreting the time property though. Will need help with that.

--- App/src/main/resources/static/js/portcallmessages.js
+++ App/src/main/resources/static/js/portcallmessages.js
@@ -2,15 +2,12 @@ var queueId;
 
 //Start subscription by getting a queueID
 window.onload = function () {
-    function startSubscription() {
-        $.ajax({
-            url: "http://localhost:8080/queue/subscribe",
-            context: document.body
-        }).done(function(data) { //When response is recieved
-            $('#queueId').text(data)
-            queueId = $('#queueId').text();
-        });
-    }
+    $.ajax({
+        url: "http://localhost:8080/queue/subscribe",
+        context: document.body
+    }).done(function(data) { //When response is recieved     
+        queueId = data;
+    });
 
     //Look for new messages every 10 seconds
     setInterval(getNewMessages, 10000);
@@ -22,7 +19,8 @@ function getNewMessages(){
        context: document.body
    }).done(function(data) {
        data.forEach(function (m){
-        $("#portcallmessages").prepend(getMessageContainer(m.portCallId + " 05:23:" + new Date().getSeconds(),m.comment));
+        console.log(m.serviceState);
+        $("#portcallmessages").prepend(getMessageContainer(getCurrentDate(), "unknown", m.serviceState.timeType, m.serviceState.serviceObject, m.serviceState.time));
        });
     });
 }
@@ -31,18 +29,25 @@ function addTestMessage(){
     $("#portcallmessages").prepend(getMessageContainer("Tug 05:23:" + new Date().getSeconds(),"New ETB 22:12."));
 }
 
-function getMessageContainer(headerText, message){
+function getMessageContainer(timeReceived, sender, timeType, serviceObject, time){
     var container = document.createElement("div");
     container.classList.add("portcallmessage");
-    var header = document.createElement("h4");
-    var headerText = document.createTextNode(headerText);
+    var header = document.createElement("b");
+    var headerText = document.createTextNode(timeReceived + " from " + sender);
     header.appendChild(headerText);
     container.appendChild(header);
 
-    var messageHeader = document.createElement("b");
-    messageHeader.innerHTML = "Message: ";
-    container.appendChild(messageHeader);
-    container.appendChild(document.createTextNode(message));
+    var typeAndObject = document.createTextNode("New " + timeType.toLowerCase() + " " + serviceObject.toLowerCase());
+    var newTime = document.createTextNode(time);
+    container.appendChild(document.createElement("br"));
+    container.appendChild(typeAndObject);
+    container.appendChild(document.createElement("br"));
+    container.appendChild(newTime);
 
     return container;
+}
+
+function getCurrentDate(){
+    var currentDate = new Date();
+    return currentDate.getFullYear() + "-" + currentDate.getMonth() + "-" + currentDate.getDate() + " " + currentDate.getHours() + ":" + currentDate.getMinutes() + ":" + currentDate.getSeconds();
 }
