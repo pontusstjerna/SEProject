98d0678e0a1038200e612b1614abcf6c7abe65e7
mikiswahn <miki.swahn@gmail.com>
2017-05-08 18:11:36 +0200

    ability to fetch data from backend to frontend

--- App/src/main/resources/static/js/unscheduled.js
+++ App/src/main/resources/static/js/unscheduled.js
@@ -1,5 +1,8 @@
 var addPortCallButton = $('#add-portcall-button');
 var addPortCallForm = $('.add-portcall-input');
+var portCallList = $('#list-of-portcalls');
+
+$('#load-port-calls').on('click',getAllPortCalls);
 
 addPortCallButton.on('click', function () {
     addPortCallForm.removeClass('hidden');
@@ -24,3 +27,23 @@ addPortCallForm.on('submit', function(){
 
     return false;
 });
+
+function getAllPortCalls(){
+ $.ajax({
+     url: 'http://localhost:8080/portcalls',
+     type: 'GET',
+     success: updatePortCallList
+ });
+}
+
+function updatePortCallList(listOfPortCalls, textStatus){
+    portCallList.empty();
+    for (i = 0; i < listOfPortCalls.length; i++){
+        portCallList.append('<li>' +
+            '<ul>' +
+            '<li>'+ 'cargo in: ' + listOfPortCalls[i].cargoIn +'</li>' +
+            '<li>'+  'cargo out: ' + listOfPortCalls[i].cargoOut +'</li>' +
+            '</ul>' +
+            '</li>')
+    }
+}
